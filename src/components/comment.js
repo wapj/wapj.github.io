dd8a3111f019796a2e84c7fde17c2ab7973e6051
andy.sg <andy@kakaopage.com>
2021-08-11 18:44:39 +0900

    댓글 설정 잘못된거 수정... (댓글 다날라가겠다..ㅠㅠ )

--- src/components/comment.js
+++ src/components/comment.js
@@ -7,7 +7,7 @@ export default class Comments extends Component {
     script.setAttribute("src", "https://utteranc.es/client.js")
     script.setAttribute("crossorigin", "anonymous")
     script.setAttribute("async", true)
-    script.setAttribute("repo", "wapj/blog")
+    script.setAttribute("repo", "wapj/wapj.github.io")
     script.setAttribute("issue-term", "pathname")
     script.setAttribute("theme", "github-light")
     anchor.appendChild(script)
